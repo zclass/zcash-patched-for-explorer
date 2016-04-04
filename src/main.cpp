857e17a7ceb2d192c68fb963c1fda3b0f81d6d0f
Braydon Fuller <courier@braydon.com>
2016-04-04 16:37:43 -0400

    main: mempool address index
    Zcash: Integrated into older mempool code

--- src/main.cpp
+++ src/main.cpp
@@ -1597,6 +1597,9 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransa
 
         // Store transaction in memory
         pool.addUnchecked(hash, entry, !IsInitialBlockDownload());
+        if (fAddressIndex) {
+            pool.addAddressIndex(entry, view);
+        }
     }
 
     SyncWithWallets(tx, NULL);
