3d4431a1766d57f1015332becf7b65321dbe92d6
Braydon Fuller <braydon@bitpay.com>
2016-06-01 14:57:39 -0400

    main: do not log error when spent info not found

--- src/main.cpp
+++ src/main.cpp
@@ -1615,7 +1615,7 @@ bool GetSpentIndex(CSpentIndexKey &key, CSpentIndexValue &value)
         return true;
 
     if (!pblocktree->ReadSpentIndex(key, value))
-        return error("unable to get spent info");
+        return false;
 
     return true;
 }
